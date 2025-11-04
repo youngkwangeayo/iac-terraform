1139f9ea008280f399231956696fb111213a60ad
hugo <youngkwang.hugo@gmail.com>
2025-11-04 16:06:26 +0900

    구조 변경

---
+++ tests/ecs-test/main.tf
@@ -0,0 +1,46 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.18.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+
+module "aws_ecs_service" {
+  source              = "../../infra/dev/modules/ecs/ecs-service"
+  name                = "test-svc"
+  cluster_id          = "arn:aws:ecs:ap-northeast-2:123456789012:cluster/test"
+  task_definition_arn = "arn:aws:ecs:ap-northeast-2:123456789012:task-definition/test:1"
+  desired_count       = 1
+  launch_type         = "FARGATE"
+  platform_version    = "LATEST"
+  scheduling_strategy = "REPLICA"
+
+  network_configuration = {
+    subnets          = ["subnet-xxx"]
+    security_groups  = ["sg-xxx"]
+    assign_public_ip = true
+  }
+
+  capacity_provider_strategy = []
+  load_balancer              = null
+
+  deployment_configuration = {
+    maximum_percent         = 200
+    minimum_healthy_percent = 100
+    deployment_circuit_breaker = {
+      enable   = true
+      rollback = true
+    }
+  }
+
+  tags = {}
+
+}
